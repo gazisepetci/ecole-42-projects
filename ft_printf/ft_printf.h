25856015f0483efcd6304cf0cff7d9d4c614f630
gazisepetci <77507911+gazisepetci@users.noreply.github.com>
2024-05-09 21:58:00 +0300

    Add files via upload

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,19 @@
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft/libft.h"
+# include <stdarg.h>
+
+int	ft_basamak_sayisi(unsigned int c);
+int	ft_uzunluk(unsigned long sayi);
+int	ft_putchars(char c);
+int	hexfunc(unsigned int sayi, char c);
+int	printing(va_list liste, int tip);
+int	ft_printf(const char *str, ...);
+int	ft_uitoa(unsigned int n);
+int	ft_array(char *str);
+int	ptr_printer(unsigned long n);
+int	ptr_print(unsigned long n);
+
+#endif
